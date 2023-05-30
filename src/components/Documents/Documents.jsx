66a93b5f5ee75b37e193b45c1e83b49fcc9813fe
juancarcaicedo <juancarcaicedo@unicauca.edu.co>
2023-05-29 23:17:16 -0500

    FormularioDocumento

--- src/components/Documents/Documents.jsx
+++ src/components/Documents/Documents.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { environment } from "../../hooks/environment";
 import style from "./Documents.module.css";
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
-import { faFileExcel } from '@fortawesome/free-solid-svg-icons';
-import { faFileWord } from '@fortawesome/free-solid-svg-icons';
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { faFileExcel } from "@fortawesome/free-solid-svg-icons";
+import { faFileWord } from "@fortawesome/free-solid-svg-icons";
 import AddIcon from "@material-ui/icons/Add";
 import SearchIcon from "@material-ui/icons/Search";
 import BlockIcon from "@material-ui/icons/Block";
 import GetAppIcon from "@material-ui/icons/GetApp";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import Modals from "../Modals/Modals";
 
 function Documents() {
   const [documentos, setDocumentos] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const url = environment.url + "/api/documentos/listado?id_escenario=2";
+  const [modalContent, setModalContent] = useState("");
+  const [modalTitle, setModalTitle] = useState("");
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     fetch(url)
@@ -28,7 +32,7 @@ function Documents() {
   const filteredDocuments = documentos.filter((documento) =>
     documento.nombre_archivo.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
+  
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     const year = date.getFullYear();
@@ -49,13 +53,28 @@ function Documents() {
   const getIconByExtension = (extension) => {
     switch (extension) {
       case "pdf":
-        return <FontAwesomeIcon style={{ color: "#980c0f" ,fontSize: "30px" }} icon={faFilePdf} />;
+        return (
+          <FontAwesomeIcon
+            style={{ color: "#980c0f", fontSize: "30px" }}
+            icon={faFilePdf}
+          />
+        );
       case "doc":
       case "docx":
-        return <FontAwesomeIcon style={{ color: "#0a2167", fontSize: "30px" }} icon={faFileWord} />;
+        return (
+          <FontAwesomeIcon
+            style={{ color: "#0a2167", fontSize: "30px" }}
+            icon={faFileWord}
+          />
+        );
       case "xls":
       case "xlsx":
-        return <FontAwesomeIcon style={{ color: "#007a29", fontSize: "30px" }} icon={faFileExcel} />;
+        return (
+          <FontAwesomeIcon
+            style={{ color: "#007a29", fontSize: "30px" }}
+            icon={faFileExcel}
+          />
+        );
       default:
         return null;
     }
@@ -73,11 +92,27 @@ function Documents() {
     //codigo para eliminar el documento
   };
 
+  const handleOpenDocument = () => {
+    setModalContent("NewDocument");
+    setModalTitle("Agregar Documento");
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={style.containerDocuments}>
+      <Modals
+        open={open}
+        handleClose={handleClose}
+        modalContent={modalContent}
+        title={modalTitle}
+      />
       <div className={style.containerSearchBar}>
         <div className={style.buttonNewDocument}>
-          <button className={style.buttonND}>
+          <button className={style.buttonND} onClick={handleOpenDocument}>
             {" "}
             <i>
               <AddIcon style={{ fontSize: "15px" }} />
@@ -109,27 +144,27 @@ function Documents() {
       </div>
       <div className={style.containerListDocs}>
         <ul>
-        {filteredDocuments.length > 0 ? (
-          filteredDocuments.map((documento) => (
-            <li key={documento.id_documento}>
-              <div>
-                {getIconByExtension(documento.extension)}
-                {documento.nombre_archivo}
-              </div>
-              <div>{formatDate(documento.fecha_vigencia)}</div>
-              <div>
-                <button onClick={() => handleDownload(documento)}>
-                  <GetAppIcon style={{ color: "#0a2167" }} />
-                </button>
-                <button onClick={() => handleEdit(documento)}>
-                  <EditIcon style={{ color: "#0a2167" }} />
-                </button>
-                <button onClick={() => handleDelete(documento)}>
-                  <DeleteForeverIcon style={{ color: "#980c0f" }} />
-                </button>
-              </div>
-            </li>
-          ))
+          {filteredDocuments.length > 0 ? (
+            filteredDocuments.map((documento) => (
+              <li key={documento.id_documento}>
+                <div>
+                  {getIconByExtension(documento.extension)}
+                  {documento.nombre_archivo}
+                </div>
+                <div>{formatDate(documento.fecha_vigencia)}</div>
+                <div>
+                  <button onClick={() => handleDownload(documento)}>
+                    <GetAppIcon style={{ color: "#0a2167" }} />
+                  </button>
+                  <button onClick={() => handleEdit(documento)}>
+                    <EditIcon style={{ color: "#0a2167" }} />
+                  </button>
+                  <button onClick={() => handleDelete(documento)}>
+                    <DeleteForeverIcon style={{ color: "#980c0f" }} />
+                  </button>
+                </div>
+              </li>
+            ))
           ) : (
             <div className={style.noFound}>
               <BlockIcon />
