47568dd869dd92eecadec7aabab72f3b6f4c07f5
sebastian <tatan873@gmail.com>
2023-05-31 23:04:09 -0500

    update get info calendar

--- src/components/Modals/EditDocument.jsx
+++ src/components/Modals/EditDocument.jsx
@@ -6,13 +6,12 @@ import { es } from "date-fns/locale";
 import style from "./document.module.css";
 import { json } from "react-router-dom";
 
-export default function NewDocument({onClose, documentData}) {
+export default function NewDocument({ onClose, documentData }) {
   console.log("EDITAR DOCUMENTO");
-  console.log(documentData);
+  console.log(documentData.fecha_vigencia);
   // funciones para el control de arhivo
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(/* { name: "nuevo archivo" } */[]);
   const [saveFile, setSaveFile] = useState(null);
-
 
   const [selectedEscenaryId, setSelectedEscenaryId] = useState("");
   const [escenary, setEscenary] = useState([]);
@@ -33,8 +32,7 @@ export default function NewDocument({onClose, documentData}) {
       });
   }, []);
 
-
-  const doc = {
+  /*   const doc = {
     id_documento: 2,
     id_archivo: 2,
     id_escenario: 2,
@@ -43,22 +41,19 @@ export default function NewDocument({onClose, documentData}) {
     tipo_documento: "Plan de practicas",
     extension: "pdf",
   };
-
-
-/*   with useEfect with an empty array will only run when starting the component [] */
-const [loading, setloading] = useState("");
-const [auxEscenary, setauxEscenary] = useState("")
+ */
+  /*   with useEfect with an empty array will only run when starting the component [] */
+  const [loading, setloading] = useState("");
+  const [auxEscenary, setauxEscenary] = useState("");
   useEffect(() => {
-    setloading(doc);
-    setFechaVigencia(new Date("2030-12-08T05:00:00.000+00:00")); 
-    const selectedId = 2;
+    setloading(documentData);
+    setFechaVigencia(new Date(documentData.fecha_vigencia));
     if (escenary.length > 0) {
-      setauxEscenary(escenary[loading.id_escenario]);
+      setauxEscenary(escenary[documentData.id_escenario]);
       console.log("PROBANDO");
       console.log(auxEscenary.nombre);
     }
   }, [escenary]);
-  
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -98,7 +93,7 @@ const [auxEscenary, setauxEscenary] = useState("")
     setTipeDocument(event.target.value);
   };
   //
- 
+
   const handleEscenaryChange = (event) => {
     setSelectedEscenaryId(event.target.value);
   };
@@ -157,26 +152,49 @@ const [auxEscenary, setauxEscenary] = useState("")
     <form onSubmit={handleSubmitCreateDocument} id="myForm">
       <div className={style.selectContainer}>
         <div className={style.selectWrapper}>
-          <h4>  <span className={style.numberRounded}>1</span>PASO 1: Seleccionar Tipo de Documento  </h4>
-          <h5> Tipo de documento <span className={style.fieldPriority}>*</span>   </h5>
-          <select className={style.customSelect} onChange={saveTipoDocument} value={tipeDocument}
+          <h4>
+            {" "}
+            <span className={style.numberRounded}>1</span>PASO 1: Seleccionar
+            Tipo de Documento{" "}
+          </h4>
+          <h5>
+            {" "}
+            Tipo de documento <span className={style.fieldPriority}>
+              *
+            </span>{" "}
+          </h5>
+          <select
+            className={style.customSelect}
+            onChange={saveTipoDocument}
+            value={tipeDocument}
           >
-            <option hidden defaultValue> {loading.tipo_documento} </option> {/*shows the plan when loading */}
+            <option hidden defaultValue>
+              {" "}
+              {loading.tipo_documento}{" "}
+            </option>{" "}
+            {/*shows the plan when loading */}
             <option>Plan de Prácticas</option>
             <option>Plan de Prácticas 2</option>
             <option>Plan de Prácticas 3</option>
           </select>
         </div>
         <div className={style.selectWrapper}>
-          <h4><span className={style.numberRounded}>2</span>PASO 2: Selecione el escenario de practicas </h4>
-          <h5> Escenario de practicas{" "}<span className={style.fieldPriority}>*</span></h5>
+          <h4>
+            <span className={style.numberRounded}>2</span>PASO 2: Selecione el
+            escenario de practicas{" "}
+          </h4>
+          <h5>
+            {" "}
+            Escenario de practicas{" "}
+            <span className={style.fieldPriority}>*</span>
+          </h5>
           <select
             className={style.customSelect}
             onChange={handleEscenaryChange}
             value={selectedEscenaryId}
           >
-     {/*    <option hidden defaultValue> {selectedEscenari} </option>  */}
-          <option value="2">{auxEscenary.nombre}</option>
+            {/*    <option hidden defaultValue> {selectedEscenari} </option>  */}
+            <option value="2">{auxEscenary.nombre}</option>
             {escenary.map((item) => (
               <option key={item.id} value={item.id}>
                 {item.nombre}
@@ -194,7 +212,8 @@ const [auxEscenary, setauxEscenary] = useState("")
             Fecha de Vigencia
           </h4>
           <label htmlFor="fechaInicio"></label>
-          <DatePicker  id="fechaInicio"
+          <DatePicker
+            id="fechaInicio"
             selected={fechaVigencia}
             onChange={(date) => setFechaVigencia(date)}
             dateFormat="yyyy-MM-dd"
@@ -204,23 +223,24 @@ const [auxEscenary, setauxEscenary] = useState("")
           />
         </div>
 
+        {/* EDITAR documento */}
         <div className={style.selectWrapper}>
-          <h4> <span className={style.numberRounded}>4</span>PASO 4: Subir  Documento </h4>
+          <h4>
+            {" "}
+            <span className={style.numberRounded}>4</span>PASO 4: Subir
+            Documento{" "}
+          </h4>
           <div className={style.fileUploadContainer}>
-            
-            {selectedFile ? (
+           {/*  {selectedFile ? ( */}
               <p>Archivo seleccionado: {selectedFile.name}</p>
-            ) : (
-              <div>
-               {/*   {getIconByExtension(documentData.extension)} */}
-                <p>Arrastre el archivo aquí o haga clic para seleccionarlo.</p>
-              </div>
-            )}
-            <input
-              type="file"
-              id="fileInput"
-              onChange={handleFileChange}
-              onDrop={handleDrop}
+            /* ) : ( */
+            <div>
+                {/* {getIconByExtension(documentData.extension)} */}
+                <p>Arrastre el archivo aquí o haga clic para seleccionarlo.</p> </div> 
+           /*      )} */
+
+              <input type="file" id="fileInput"  onChange={handleFileChange}  
+            onDrop={handleDrop}
               onDragOver={handleDragOver}
             />
           </div>
@@ -230,7 +250,9 @@ const [auxEscenary, setauxEscenary] = useState("")
       {alertMessage && <span className="alert">{alertMessage}</span>}
 
       <center>
-        <button className={style.buttonStyle} type="submit">Editar Documento  </button>
+        <button className={style.buttonStyle} type="submit">
+          Editar Documento{" "}
+        </button>
       </center>
     </form>
   );
